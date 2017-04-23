b8a6422434f172ef84018069050e1c2504a8dbfb
Laurynas Sakalauskas <laurynas@helix.lt>
2017-04-23 16:00:31 +0100

    Add support for ref creation

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ module.exports = class SentryPlugin {
     this.apiKey = options.apiKey
 
     this.releaseVersion = options.release
+    this.ref = options.ref
 
     this.include = options.include || DEFAULT_INCLUDE
     this.exclude = options.exclude
@@ -111,7 +112,7 @@ module.exports = class SentryPlugin {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ version: this.releaseVersion }),
+      body: JSON.stringify({ version: this.releaseVersion, ref: this.ref }),
     })
   }
 
